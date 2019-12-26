82df0f17b6ca0ac8dbfc84afcc577a345a0b4ab9
Alexy102 <59171672+Alexy102@users.noreply.github.com>
2019-12-26 21:58:36 +0700

    Add files via upload

---
+++ Program3.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double znamenatel=2, symm = 1.0;
+            while(symm<1.644)
+            {
+                symm += 1 / (znamenatel * znamenatel);
+                znamenatel++;
+            }
+            Console.WriteLine(symm);
+        }
+    }
+}
